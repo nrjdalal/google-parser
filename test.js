330e7c4b4ce7b3a2b7f2af010b7c56bb890f9af2
Neeraj Dalal <admin@nrjdalal.com>
2023-06-20 11:39:02 +0530

    cleanup

--- test.js
+++
@@ -1,6 +0,0 @@
-import { getHeaders, googleSearch } from '@nrjdalal/google-parser'
-
-const headers = getHeaders()
-
-console.log(await googleSearch({ query: 'apple', options: { headers } }))
-console.log(await googleSearch({ query: 'microsoft', options: { headers } }))
